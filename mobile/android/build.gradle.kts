82df1ce3301d518620a68e8df5fa61e8f01625d9
sujayamindev <sujaya.manith23@gmail.com>
2026-05-04 01:41:42 +0530

    refactor(build): update Kotlin language settings in subprojects

--- mobile/android/build.gradle.kts
+++ mobile/android/build.gradle.kts
@@ -14,6 +14,13 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+    
+    // Override outdated Kotlin language settings from old plugins
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions {
+            languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
+        }
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
